3a360140a97f6f4627269b1dbf2b3ddf9f8195b9
Edu <eddu290@gmail.com>
2021-07-30 15:46:04 +0200

    Populate recyclerView

--- app/src/main/java/com/actrabajoequipo/recipesapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/actrabajoequipo/recipesapp/ui/home/HomeFragment.kt
@@ -4,42 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.actrabajoequipo.recipesapp.R
+import androidx.fragment.app.viewModels
 import com.actrabajoequipo.recipesapp.databinding.FragmentHomeBinding
+import com.actrabajoequipo.recipesapp.ui.home.adapter.RecipesAdapter
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private val homeViewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: RecipesAdapter
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        adapter = RecipesAdapter(homeViewModel::onRecipeClicked)
+        binding.recycler.adapter = adapter
+        homeViewModel.recipes.observe(viewLifecycleOwner, {
+            adapter.recipes = it
         })
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return binding.root
     }
 }
