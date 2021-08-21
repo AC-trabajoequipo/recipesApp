2bd98defe7dfa6111eecf443b5aa8b9ff4416dac
Eduardo González <eddu290@gmail.com>
2021-08-21 13:59:50 +0200

    Rename model and param

--- app/src/main/java/com/actrabajoequipo/recipesapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/actrabajoequipo/recipesapp/ui/home/HomeViewModel.kt
@@ -4,7 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.actrabajoequipo.recipesapp.model.Recipe
+import com.actrabajoequipo.recipesapp.model.RecipeDto
 import com.actrabajoequipo.recipesapp.model.RecipesRepository
 import com.actrabajoequipo.recipesapp.ui.Scope
 import kotlinx.coroutines.launch
@@ -13,8 +13,8 @@ class HomeViewModel : ViewModel(), Scope by Scope.Impl() {
 
     private val recipesRepository: RecipesRepository by lazy { RecipesRepository() }
 
-    private val _recipes = MutableLiveData<List<Recipe>>()
-    val recipes: LiveData<List<Recipe>>
+    private val _recipes = MutableLiveData<List<RecipeDto>>()
+    val recipes: LiveData<List<RecipeDto>>
         get() {
             if (_recipes.value == null) refresh()
             return _recipes
@@ -35,7 +35,7 @@ class HomeViewModel : ViewModel(), Scope by Scope.Impl() {
         super.onCleared()
     }
 
-    fun onRecipeClicked(recipe: Recipe) {
+    fun onRecipeClicked(recipe: RecipeDto) {
         Log.d("onRecipeClicked", "Recipe ${recipe.id} was clicked")
     }
 }
