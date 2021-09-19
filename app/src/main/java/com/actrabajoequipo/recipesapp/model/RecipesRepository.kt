4b3baaefd79b33f0279c694940875da56515170f
Eduardo González <eddu290@gmail.com>
2021-09-19 13:23:31 +0200

    Update the repository with the Dao calls. Add extension mapper to map from Dto to entity

--- app/src/main/java/com/actrabajoequipo/recipesapp/model/RecipesRepository.kt
+++ app/src/main/java/com/actrabajoequipo/recipesapp/model/RecipesRepository.kt
@@ -1,8 +1,43 @@
 package com.actrabajoequipo.recipesapp.model
 
-class RecipesRepository {
+import com.actrabajoequipo.recipesapp.RecipesApp
+import com.actrabajoequipo.recipesapp.model.database.Recipe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-    suspend fun getRecipes() =
-        RecipeBook.service
-            .getRecipes()
+class RecipesRepository(application: RecipesApp) {
+
+    private val db = application.db
+
+    suspend fun getRecipes(): List<Recipe> = withContext(Dispatchers.IO) {
+        with(db.recipeDao()) {
+            if (recipeCount() <= 0) {
+                val recipes = RecipeBook.service
+                    .getRecipes()
+
+                insertRecipes(recipes.map { it.convertToDbRecipe() })
+            }
+
+            getAll()
+        }
+    }
+
+    suspend fun findById(id: String): Recipe = withContext(Dispatchers.IO) {
+        db.recipeDao().findById(id)
+    }
+
+    suspend fun update(recipe: Recipe) = withContext(Dispatchers.IO) {
+        db.recipeDao().updateRecipe(recipe)
+    }
+
 }
+
+private fun RecipeDto.convertToDbRecipe() = Recipe(
+    id,
+    name,
+    description ?: "",
+    imgUrl ?: "",
+    ingredients,
+    preparation ?: "",
+    false
+)
