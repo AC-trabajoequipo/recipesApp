647469b02c61c5a2cdf1a5f3c1611d9ad5c40084
Eduardo González <eddu290@gmail.com>
2021-10-16 11:27:29 +0200

    Add search feature to home toolbar

--- app/src/main/java/com/actrabajoequipo/recipesapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/actrabajoequipo/recipesapp/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.actrabajoequipo.recipesapp.ui.home
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.actrabajoequipo.recipesapp.R
 import com.actrabajoequipo.recipesapp.databinding.FragmentHomeBinding
 import com.actrabajoequipo.recipesapp.model.RecipesRepository
 import com.actrabajoequipo.recipesapp.ui.app
@@ -44,7 +46,54 @@ class HomeFragment : Fragment() {
         adapter = RecipesAdapter(homeViewModel::onRecipeClicked)
         binding.recycler.adapter = adapter
 
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        val menuItem = menu.findItem(R.id.search)
+        val searchView = menuItem.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrBlank()) {
+                    searchRecipes(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+
+        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                searchRecipes("")
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                setRecipesInitialState()
+                return true
+            }
+
+        })
+    }
+
+    private fun setRecipesInitialState() {
+        homeViewModel.refresh()
+    }
+
+    private fun searchRecipes(query: String) {
+        homeViewModel.doSearch(query)
     }
 
     private fun updateUI(uiModel: UIModel) {
