3f93ca55ff203367c8f9df7a522d42dad14ad300
Pablo Ingelmo <ingelmogarcia@gmail.com>
2021-12-06 21:52:34 +0100

    loginWithGoogleAccount en el ViewModel

--- app/src/main/java/com/actrabajoequipo/recipesapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/actrabajoequipo/recipesapp/ui/login/LoginViewModel.kt
@@ -1,10 +1,19 @@
 package com.actrabajoequipo.recipesapp.ui.login
 
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.actrabajoequipo.recipesapp.R
 import com.actrabajoequipo.recipesapp.server.FirebaseManager
 import com.actrabajoequipo.recipesapp.ui.Scope
+import com.actrabajoequipo.recipesapp.ui.login.usernameForGoogleAccount.UsernameForGoogleAccountActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
@@ -18,9 +27,19 @@ class LoginViewModel(
         class FillinFields : UiLogin()
     }
 
+    sealed class UiLoginWithGoogleAccount(){
+        class Success : UiLoginWithGoogleAccount()
+        class NotSuccess : UiLoginWithGoogleAccount()
+        class Error : UiLoginWithGoogleAccount()
+    }
 
     private val _logeado = MutableLiveData<UiLogin>()
     val logeado: LiveData<UiLogin> get() = _logeado
+
+    private val _logeadoGoogle = MutableLiveData<UiLoginWithGoogleAccount>()
+    val logeadoGoogle: LiveData<UiLoginWithGoogleAccount> get() = _logeadoGoogle
+
+    private val GOOGLE_SIGN_IN = 100
 
 
     init {
@@ -51,7 +70,27 @@ class LoginViewModel(
     }
 
 
+    fun postLoginWithGoogleAccount(requestCode: Int, resultCode: Int, data: Intent?){
+        if (requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
 
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if(account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    firebaseManager.fbAuth.signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            _logeadoGoogle.value = UiLoginWithGoogleAccount.Success()
+                        }else{
+                            _logeadoGoogle.value = UiLoginWithGoogleAccount.NotSuccess()
+                        }
+                    }
+                }
+            }catch (e: ApiException){
+                _logeadoGoogle.value = UiLoginWithGoogleAccount.Error()
+            }
+        }
+    }
 
 
     override fun onCleared() {
