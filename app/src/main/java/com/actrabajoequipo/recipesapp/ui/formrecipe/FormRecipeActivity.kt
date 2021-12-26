e6fff96bd22a3c839d14bb355eda90e26cd05745
mele <mele@yahoo.es>
2021-12-26 12:27:59 +0100

    added camara and request app permissions in FormRecipeActivity
    create chooser intent

--- app/src/main/java/com/actrabajoequipo/recipesapp/ui/formrecipe/FormRecipeActivity.kt
+++ app/src/main/java/com/actrabajoequipo/recipesapp/ui/formrecipe/FormRecipeActivity.kt
@@ -1,7 +1,11 @@
 package com.actrabajoequipo.recipesapp.ui.formrecipe
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
@@ -10,28 +14,51 @@ import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.actrabajoequipo.recipesapp.R
 import com.actrabajoequipo.recipesapp.databinding.ActivityFormRecipeBinding
 import com.actrabajoequipo.recipesapp.ui.app
 import com.actrabajoequipo.recipesapp.ui.getViewModel
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_form_recipe.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
-class FormRecipeActivity : AppCompatActivity() {
+class FormRecipeActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 47
+
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
 
     private lateinit var binding: ActivityFormRecipeBinding
     private val viewModel by lazy { getViewModel { component.formRecipeViewModel } }
     private lateinit var component: FormRecipeComponent
 
-    private val galleryResult =
+    private lateinit var currentPhotoPath: String
+    private lateinit var photoUri: Uri
+
+    private val resultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
                 it.data?.data.let { photoSelectedUri ->
-                    binding.photoRecipe.setImageURI(photoSelectedUri)
                     if (photoSelectedUri != null) {
+                        binding.photoRecipe.setImageURI(photoSelectedUri)
                         viewModel.uploadImage(photoSelectedUri)
+                        return@registerForActivityResult
                     }
                 }
+                binding.photoRecipe.setImageURI(photoUri)
+                viewModel.uploadImage(photoUri)
             }
         }
 
@@ -45,7 +72,7 @@ class FormRecipeActivity : AppCompatActivity() {
             setContentView(root)
             setSupportActionBar(detailToolbar)
             detailToolbar.setNavigationOnClickListener { onBackClicked() }
-            btnAddImage.setOnClickListener { openGallery() }
+            btnAddImage.setOnClickListener { getNewImage() }
         }
 
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -123,7 +150,7 @@ class FormRecipeActivity : AppCompatActivity() {
 
         viewModel.progressUploadImage.observe(this, { progress ->
             binding.pbUploadImage.progress = progress.toInt()
-            binding.tvProgressPercent.text = "$progress%"
+            binding.tvProgressPercent.text = "$progress %"
         })
 
         viewModel.imageUpload.observe(this, {
@@ -162,10 +189,84 @@ class FormRecipeActivity : AppCompatActivity() {
         overridePendingTransition(0, R.anim.slide_out);
     }
 
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        galleryResult.launch(intent)
+    private fun getNewImage() {
+        if(checkAllPermissionsGranted(REQUIRED_PERMISSIONS)){
+            launchChooser()
+            return
+        }
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
     }
+
+    private fun launchChooser() {
+        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        // intent para coger foto de la camara
+        val cameraIntent = getTakePictureIntent()
+
+        // Construimos el chooser que vamos a lanzar
+        val pickTitle = getString(R.string.choose_picture_method)
+        val chooserIntent: Intent = Intent.createChooser(pickIntent, pickTitle)
+        chooserIntent.putExtra(
+            Intent.EXTRA_INITIAL_INTENTS, arrayOf(cameraIntent)
+        )
+
+        // Lanzamos el resultLauncher
+        resultLauncher.launch(chooserIntent)
+    }
+
+    private fun getTakePictureIntent(): Intent {
+        return Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        // Creamos fichero para guardar la imagen de la camara
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        null
+                    }
+                    // Continue only if the File was successfully created
+                    // Definimos content provider en el manifest y en el .kt
+                    photoFile?.also {
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                            this,
+                            "${packageName}.provider",
+                            it
+                        )
+                        photoUri = photoURI
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    }
+                }
+            }
+        }
+
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String =
+            SimpleDateFormat("'camera_'ddMMyyyy_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun checkAllPermissionsGranted(permissions: Array<String>): Boolean {
+        return permissions.all { permission ->
+            ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (checkAllPermissionsGranted(permissions))
+            launchChooser()
+}
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_form_add_recipe, menu)
